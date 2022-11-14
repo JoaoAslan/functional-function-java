15d1f82572a39b5a4e27501065e29c1b7ecefc75
JoaoAslan <contato.joaoaslan@gmail.com>
2022-11-13 22:13:39 -0300

    Solution with Lambda Inline

--- application/Program.java
+++ application/Program.java
@@ -1,11 +1,9 @@
 package aula5.application;
 
 import aula5.entities.Product;
-import aula5.util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -18,9 +16,7 @@ public class Program {
         list.add(new Product("Tablet", 350.5));
         list.add(new Product("HD Case", 80.9));
 
-        Function<Product, String> func = p -> p.getName().toUpperCase();
-
-        List<String> names = list.stream().map(func).collect(Collectors.toList());
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 
         names.forEach(System.out::println);
     }
--- util/UpperCaseName.java
+++ util/UpperCaseName.java
@@ -4,6 +4,7 @@ import aula5.entities.Product;
 
 import java.util.function.Function;
 
+// https://docs.oracle.com/javase/10/docs/api/java/util/function/Function.html
 public class UpperCaseName implements Function<Product, String> {
 
     @Override
