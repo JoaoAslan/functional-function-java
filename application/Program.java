601a4de3d51c8fa8a19c3a0ad9e8dfe4adedcea5
JoaoAslan <contato.joaoaslan@gmail.com>
2022-11-13 22:08:28 -0300

    Solution with interface Function

---
+++ application/Program.java
@@ -0,0 +1,24 @@
+package aula5.application;
+
+import aula5.entities.Product;
+import aula5.util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.0));
+        list.add(new Product("Mouse", 50.0));
+        list.add(new Product("Tablet", 350.5));
+        list.add(new Product("HD Case", 80.9));
+
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+
+        names.forEach(System.out::println);
+    }
+}
---
+++ util/UpperCaseName.java
@@ -0,0 +1,13 @@
+package aula5.util;
+
+import aula5.entities.Product;
+
+import java.util.function.Function;
+
+public class UpperCaseName implements Function<Product, String> {
+
+    @Override
+    public String apply(Product p) {
+        return p.getName().toUpperCase();
+    }
+}
