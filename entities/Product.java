a33c0a1e2511cf6a3fbf6a6d028539433d50f95a
JoaoAslan <contato.joaoaslan@gmail.com>
2022-11-13 22:09:36 -0300

    Solution with Reference Method (static)

--- entities/Product.java
+++ entities/Product.java
@@ -26,6 +26,10 @@ public class Product {
         this.price = price;
     }
 
+    public static String staticUpperCaseName(Product p) {
+        return p.getName().toUpperCase();
+    }
+
     @Override
     public String toString() {
         return getName() + String.format(", $%.2f", getPrice());
